4bc3e5b065e45f57db1d2fb085050c51d5d5bff1
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-02-02 21:10:10 -0600

    Add files via upload

---
+++ Hm1PROGR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hm1progr;
+
+/**
+ *
+ * @author rener
+ */
+public class Hm1PROGR {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        Persona Persol;//declaro un identificador (variable) de tipo Persona
+        //solo he declarado la variable, no he creado el objeto
+        //Crear el objeto (instanciar) consiste en asignar memoria.
+        int valor;
+    }
+
+}
+
+//Clase PERSONA
+/*
+//NOMBREDELACLASE --> Nomenclatura Came Case
+Class NombreDeLaClase ( 'Cuerpo de la claSe )
+*/
+class Persona{
+    
+}
