f708776fb65b226d5505cb71c66a8bf90251903b
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-02-16 15:21:43 -0600

    Add files via upload

---
+++ Eva_5_Clase_Persona/src/main/java/com/mycompany/eva_5_clase_persona/Eva_5_Clase_Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_5_clase_persona;
+
+/**
+ *
+ * @author rener
+ */
+public class Eva_5_Clase_Persona {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
