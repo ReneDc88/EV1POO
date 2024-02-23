3b43726c3f4d480d76d2b8e422854d224e3c4ffb
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-02-22 18:46:30 -0600

    Add files via upload

---
+++ Eva_10_triangulo/src/main/java/com/mycompany/eva_10_triangulo/Eva_10_triangulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_10_triangulo;
+
+/**
+ *
+ * @author rener
+ */
+public class Eva_10_triangulo {
+
+    public static void main(String[] args) {
+        triangulo trian = new triangulo();
+        trian.setBase(10);
+        trian.setAltura(20);
+        trian.imprimirDatos();
+        
+    }
+}
