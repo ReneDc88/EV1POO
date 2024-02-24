1376aa263315387962a321da6558c5067e033699
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-02-23 22:09:05 -0600

    Add files via upload

---
+++ EVA1_4_MODIFICADORES_ACCESO/src/main/java/Paquete_1/a.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete_1;
+
+import Paquete_2.b;
+import Paquete_2.c;
+
+/**
+ *
+ * @author nacho
+ */
+public class a {
+    
+public static void main(String[] args) {    
+       one obj1 = new one ();
+        System.out.println(obj1);
+        
+        //clases del paquete 2
+        b objb = new b ();
+        System.out.println(objb);
+       
+        //Two obj2 =new two
+        
+        // Three obj3 = new Three();
+        c objc = new c ();
+        System.out.println(objc);
+        
+    }
+}
+
+class one{
+    
+}
