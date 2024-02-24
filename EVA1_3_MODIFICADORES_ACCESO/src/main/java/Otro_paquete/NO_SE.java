1667086d619cef1c65bdfb7c0a0ce2f56fa41663
ReneDc88 <158646500+ReneDc88@users.noreply.github.com>
2024-02-23 22:13:01 -0600

    Add files via upload

---
+++ EVA1_3_MODIFICADORES_ACCESO/src/main/java/Otro_paquete/NO_SE.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Otro_paquete;
+
+/**
+ *
+ * @author nacho
+ */
+public class NO_SE {
+ public int x;
+   protected int y;
+   private int z;
+   int w;   
+}
+
+class PruebaB{
+public int x;
+   protected int y;
+   private int z;
+   int w;
+}
---
+++ EVA1_3_MODIFICADORES_ACCESO/src/main/java/com/mycompany/eva1_3_modificadores_acceso/EVA1_3_MODIFICADORES_ACCESO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_modificadores_acceso;
+
+/**
+ *
+ * @author nacho
+ */
+public class EVA1_3_MODIFICADORES_ACCESO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
